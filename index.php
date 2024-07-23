18ea1203f587c07c848cdfdac61628b8ac9af346
evgentos <evgentos2009@gmailcom>
2024-07-23 21:24:51 +0300

    Lesson 3 finished

--- index.php
+++ index.php
@@ -2,18 +2,29 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Twig\Loader\FilesystemLoader;
+use Twig\Environment;
 
 require __DIR__ . '/vendor/autoload.php';
 
+
+$loader = new FilesystemLoader('templates');
+$view = new Environment($loader);
+
+
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
+$app->get('/', function (Request $request, Response $response, $args) use ($view) {
+    $body = $view->render('index.twig');
+    $response->getBody()->write($body);
     return $response;
 });
 
-$app->get('/about', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("This is my personal Blog!");
+$app->get('/about', function (Request $request, Response $response, $args) use ($view) {
+    $body = $view->render('about.twig', [
+        'name'=> 'Vasiliy',
+    ]);
+    $response->getBody()->write($body);
     return $response;
 });
 
